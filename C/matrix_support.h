f65675da06f28ebeb9a10901b28a7b7a67b8cb03
Xiaohe Cheng <xhspCheng@gmail.com>
2018-06-26 10:17:17 -0400

    Added matrix macro

---
+++ C/matrix_support.h
@@ -0,0 +1,21 @@
+/*
+ * Define matrix:
+ ** Matrix A;
+ ** A.width = w;
+ ** A.height = h;
+ ** A.elements = (float*)malloc(A.width * A.height * sizeof(float));
+ * Access elements:
+ ** A.elements[getIndex(i, j, A.width, A.height)] = x;
+ */
+
+int getIndex(int i, int j, int width, int height){
+    return i + (j-1) * height;
+}    
+
+typedef struct {
+    int width;
+    int height;
+    float* elements;
+} Matrix;
+
+
