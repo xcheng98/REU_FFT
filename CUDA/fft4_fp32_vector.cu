502572a95912cea64d10e75237529a806aeccae8
Xiaohe <xchengaj@connect.ust.hk>
2018-07-10 15:01:56 -0400

    Corrected fft4 bug and uploaded result

--- CUDA/fft4_fp32_vector.cu
+++ CUDA/fft4_fp32_vector.cu
@@ -15,12 +15,12 @@
 
 // CUDA includes
 #include <cuda_runtime.h>
-#include <cublas.h>
+#include <cublas_v2.h>
 
 // Matrix and vector
-#include <helper/my_vector.h>
-#include <helper/my_matrix.h>
-#include <helper/my_const.h>
+#include "helper/my_vector.h"
+#include "helper/my_matrix.h"
+#include "helper/my_const.h"
 
 float UPPER_BOUND = 1000.0f;
 
@@ -79,7 +79,7 @@ FFT_S fft4(fft::VectorF X_re, fft::VectorF X_im, fft::VectorF FX_re, fft::Vector
 {
     cublasStatus_t status;
     cublasHandle_t handle;
-    float* dev_FM, dev_input, dev_result1, dev_result2;
+    float *dev_FM, *dev_input, *dev_result1, *dev_result2;
     float alpha = 1.0f, beta = 0.0f;
 
     // Initialize cublas and allocate device memory
@@ -91,22 +91,22 @@ FFT_S fft4(fft::VectorF X_re, fft::VectorF X_im, fft::VectorF FX_re, fft::Vector
     if (cudaMalloc(reinterpret_cast<void **>(&dev_FM), 4 * 4 * sizeof(dev_FM[0])) !=
       cudaSuccess) {
         fprintf(stderr, "!!!! device memory allocation error (allocate Fourier Matrix)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
     if (cudaMalloc(reinterpret_cast<void **>(&dev_input), 4 * 2 * sizeof(dev_input[0])) !=
       cudaSuccess) {
         fprintf(stderr, "!!!! device memory allocation error (allocate Input Matrix)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
     if (cudaMalloc(reinterpret_cast<void **>(&dev_result1), 4 * 2 * sizeof(dev_result1[0])) !=
       cudaSuccess) {
         fprintf(stderr, "!!!! device memory allocation error (allocate result 1 Matrix)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
     if (cudaMalloc(reinterpret_cast<void **>(&dev_result2), 4 * 2 * sizeof(dev_result2[0])) !=
       cudaSuccess) {
         fprintf(stderr, "!!!! device memory allocation error (allocate result 2 Matrix)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
 
     // F4_re * (X_re, X_im)
@@ -114,17 +114,17 @@ FFT_S fft4(fft::VectorF X_re, fft::VectorF X_im, fft::VectorF FX_re, fft::Vector
     status = cublasSetVector(4 * 4, sizeof(F4_re.array[0]), F4_re.array, 1, dev_FM, 1);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! device access error (write F4_re)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
     status = cublasSetVector(4, sizeof(X_re.array[0]), X_re.array, 1, dev_input, 1);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! device access error (write X_re)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
-    status = cublasSetVector(4, sizeof(X_im.array[0]), X_im.array, 1, dev_input + 4 * sizeof(dev_input[0]), 1);
+    status = cublasSetVector(4, sizeof(X_im.array[0]), X_im.array, 1, dev_input + 4, 1);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! device access error (write X_im)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
     //// Call cublas gemm
     status = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 4, 2, 4, &alpha, dev_FM,
@@ -139,7 +139,7 @@ FFT_S fft4(fft::VectorF X_re, fft::VectorF X_im, fft::VectorF FX_re, fft::Vector
     status = cublasSetVector(4 * 4, sizeof(F4_im.array[0]), F4_im.array, 1, dev_FM, 1);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! device access error (write F4_im)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
     //// Call cublas gemm
     status = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 4, 2, 4, &alpha, dev_FM,
@@ -151,13 +151,13 @@ FFT_S fft4(fft::VectorF X_re, fft::VectorF X_im, fft::VectorF FX_re, fft::Vector
 
     // Combine and get result, store in result1
     alpha = -1.0f;
-    status = cublasSaxpy(handle, 4, result2 + 4 * sizeof(result2[0]), 1, result1, 1);
+    status = cublasSaxpy(handle, 4, &alpha, dev_result2 + 4, 1, dev_result1, 1);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! CUBLAS kernel execution error (ac - bd).\n");
         return FFT_FAILURE;
     }
     alpha = 1.0f;
-    status = cublasSaxpy(handle, 4, result2, 1, result1 + 4 * sizeof(result1[0]), 1);
+    status = cublasSaxpy(handle, 4, &alpha, dev_result2, 1, dev_result1 + 4, 1);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! CUBLAS kernel execution error (ad + bc).\n");
         return FFT_FAILURE;
@@ -167,35 +167,35 @@ FFT_S fft4(fft::VectorF X_re, fft::VectorF X_im, fft::VectorF FX_re, fft::Vector
     status = cublasGetVector(4, sizeof(FX_re.array[0]), dev_result1, 1, FX_re.array, 1);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! device access error (read FX_re)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
-    status = cublasGetVector(4, sizeof(FX_im.array[0]), dev_result1 + 4 * sizeof(dev_result1[0]), 1, FX_im.array, 1);
+    status = cublasGetVector(4, sizeof(FX_im.array[0]), dev_result1 + 4, 1, FX_im.array, 1);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! device access error (read FX_im)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
 
     // Deallocate device memory and shutdown
-    if (cudaFree(&dev_FM)) != cudaSuccess) {
+    if (cudaFree(dev_FM) != cudaSuccess) {
         fprintf(stderr, "!!!! device memory free error (free Fourier Matrix)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
-    if (cudaFree(&dev_input)) != cudaSuccess) {
+    if (cudaFree(dev_input) != cudaSuccess) {
         fprintf(stderr, "!!!! device memory free error (free Input Matrix)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
-    if (cudaFree(&dev_result1)) != cudaSuccess) {
+    if (cudaFree(dev_result1) != cudaSuccess) {
         fprintf(stderr, "!!!! device memory free error (free result 1 Matrix)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
-    if (cudaFree(&dev_result2)) != cudaSuccess) {
+    if (cudaFree(dev_result2) != cudaSuccess) {
         fprintf(stderr, "!!!! device memory free error (free result 2 Matrix)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
     status = cublasDestroy(handle);
     if (status != CUBLAS_STATUS_SUCCESS) {
         fprintf(stderr, "!!!! shutdown error (A)\n");
-        return EXIT_FAILURE;
+        return FFT_FAILURE;
     }
 
     return FFT_SUCCESS;
@@ -204,27 +204,27 @@ FFT_S fft4(fft::VectorF X_re, fft::VectorF X_im, fft::VectorF FX_re, fft::Vector
 int main()
 {
     FFT_S status;
-    status = init_f4();
+    status = init_F4();
     if (status != FFT_SUCCESS){
         printf("Error in Fourier matrix initialization\n");
         exit(1);
     }
 
     fft::VectorF X_re;
-    X_re.size = SIZE;
+    X_re.size = 4;
     X_re.array = (float*)malloc(X_re.size * sizeof(float));
 
     fft::VectorF X_im;
-    X_im.size = SIZE;
+    X_im.size = 4;
     X_im.array = (float*)malloc(X_im.size * sizeof(float));
 
     fft::VectorF FX_re;
-    FX_re.size = SIZE;
+    FX_re.size = 4;
     FX_re.array = (float*)malloc(FX_re.size * sizeof(float));
 
-    fft::VectorF FX_IM;
-    FX_IM.size = SIZE;
-    FX_IM.array = (float*)malloc(FX_IM.size * sizeof(float));
+    fft::VectorF FX_im;
+    FX_im.size = 4;
+    FX_im.array = (float*)malloc(FX_im.size * sizeof(float));
 
     // Setting input value
     srand(time(NULL));
@@ -237,8 +237,8 @@ int main()
 
     fft4(X_re,X_im, FX_re, FX_im);
 
-    printf("Result: \n);
-    for (int i = 0; i <= 4; i++){
+    printf("Result: \n");
+    for (int i = 1; i <= 4; i++){
         printf("FX[%d] = (%.10f, %.10f) \n", i, FX_re.element(i), FX_im.element(i));
     }
 
